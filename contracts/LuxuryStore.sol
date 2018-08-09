c16072e777762059a70b5728d4530e437fb9dfe9
ssircar <swarnima.sircar@gmail.com>
2018-08-09 03:47:13 -0400

    erc721 functionality

--- contracts/LuxuryStore.sol
+++ contracts/LuxuryStore.sol
@@ -1,52 +1,102 @@
 pragma solidity ^0.4.24;
 
-contract luxuryStore {
+import "./library/SafeMath.sol";
+import "./library/ERC721Basic.sol";
+
+contract LuxuryStore is ERC721Basic{
+
+    using SafeMath for uint256;
+
+    // =====================================================================
+    // VARIABLES
+    // =====================================================================
+
     uint256 private itemID; // Keeps track of how many items has been registered
 
-    //Only current owner
-
-    modifier onlyItemOwner(uint256 _itemNumber) {
-        require(registeredItem[_itemNumber].owners[registeredItem[_itemNumber].numOfTransfers] == msg.sender);
-        _;
-    }
-
-    modifier itemOnSale(uint256 _itemNumber) {
-        require(registeredItem[_itemNumber].forSale == true);
-        _;
-    }
-
-    mapping (uint256 => luxuryItem) registeredItem; // Maps an item's  to the metadata of that particular item
-
-    struct luxuryItem {
+    struct Item {
+        // needs unique identifier for item
         uint256 numOfTransfers;
+        bool registered; // flag to check if luxury item has been registered before
         bool forSale;
         mapping (uint256 => uint256) price; // Price of the item according to a certain transfer
         mapping (uint256 => address) owners; // Owner of an item according to a certain transfer
     }
 
-    constructor() public {
-        itemID = 0; // Set the number of items in existance to be 0
+    mapping (uint256 => Item) public items; // Maps an item's id to the metadata of that particular item
+
+    // Mapping from iteam address to owner
+    mapping (uint256 => address) internal itemOwner;
+
+
+    // =====================================================================
+    // MODIFIERS
+    // =====================================================================
+
+    // Only current owner
+    modifier onlyItemOwner(uint256 _itemNumber) {
+        require(items[_itemNumber].owners[items[_itemNumber].numOfTransfers] == msg.sender);
+        _;
     }
 
+    //?
+    modifier itemOnSale(uint256 _itemNumber) {
+        require(items[_itemNumber].forSale == true);
+        _;
+    }
+
+    // =====================================================================
+    // CONSTRUCTOR
+    // =====================================================================
+
+    /**
+    @dev Initialize the LuxuryStore contract by setting the number of items in existence as 0
+    */
+    constructor() public {
+        itemID = 0;
+    }
+
+    /**
+    @dev Quasi contstructor is called after contract is deployed
+    @param _distributeToken Address of LuxuryStore contract
+    */
+
+    function init(address _luxuryStore) public {
+        require(address(luxuryStore) == 0);
+        luxuryStore = LuxuryStore(_luxuryStore);
+    }
+
+  // =====================================================================
+  // ITEM
+  // =====================================================================
+
     function registerItem(uint256 _itemPrice) public {
-        registeredItem[itemID].numOfTransfers = 0;  // initializing the number of transfers from this item is 0
-        registeredItem[itemID].forSale = true;
-        registeredItem[itemID].price[0] = _itemPrice; // setting the price of an item
-        registeredItem[itemID].owners[0] = msg.sender; // initializing the original owner of the item is the person who sent the txn
+        require(items[itemID].registered == false);
+        items[itemID].registered = true;
+        items[itemID].numOfTransfers = 0;  // initializing the number of transfers from this item is 0
+        items[itemID].forSale = true;
+        items[itemID].price[0] = _itemPrice; // setting the price of an item
+        items[itemID].owners[0] = msg.sender; // initializing the original owner of the item is the person who sent the txn
         itemID += 1;
     }
 
     function sellItem(uint256 _itemNumber, uint256 _itemPrice) public onlyItemOwner(_itemNumber) {
-        registeredItem[_itemNumber].forSale = true;
-        registeredItem[_itemNumber].price[registeredItem[_itemNumber].numOfTransfers] = _itemPrice;
+        require(items[itemID].forSale == true);
+        /* items[_itemNumber].forSale = true; */
+        items[_itemNumber].price[items[_itemNumber].numOfTransfers] = _itemPrice;
     }
 
     function buyItem(uint256 _itemNumber) public payable onlyItemOwner(_itemNumber) itemOnSale(_itemNumber){
-        require(msg.value == registeredItem[_itemNumber].price[registeredItem[_itemNumber].numOfTransfers]); // Ether sent in must match item price listing
-        registeredItem[_itemNumber].owners[registeredItem[_itemNumber].numOfTransfers - 1].transfer(msg.value); // Sending Ether to previous owner
-        registeredItem[_itemNumber].forSale = false; // Not for sale anymore
-        registeredItem[_itemNumber].numOfTransfers += 1; // Increasing number of transfers on the item
-        registeredItem[_itemNumber].owners[registeredItem[_itemNumber].numOfTransfers] = msg.sender; // Moving ownership to item purchaser
+        require(msg.value == items[_itemNumber].price[items[_itemNumber].numOfTransfers]); // Ether sent in must match item price listing
+        items[_itemNumber].owners[items[_itemNumber].numOfTransfers - 1].transfer(msg.value); // Sending Ether to previous owner
+        items[_itemNumber].forSale = false; // Not for sale anymore
+        items[_itemNumber].numOfTransfers += 1; // Increasing number of transfers on the item
+        items[_itemNumber].owners[items[_itemNumber].numOfTransfers] = msg.sender; // Moving ownership to item purchaser
     }
+
+    // =====================================================================
+    // FALLBACK
+    // =====================================================================
+
+    function() public payable {}
 
 }
